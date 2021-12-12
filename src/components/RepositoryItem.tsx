b80815216f8573c6e7447c6f802523b74e62ca48
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-12-12 11:21:49 -0300

    feat: conexao à api do github

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,28 @@
+interface RepositoryItemProps {
+    repository: {
+        name: string;
+        description: string;
+        html_url: string;
+    };
+    className: string;
+}
+
+export default function RepositoryItem({
+    repository,
+    className,
+}: RepositoryItemProps) {
+    return (
+        <li
+            className={`block bg-white px-4 py-2 rounded-xl shadow ${className}`}
+        >
+            <strong>{repository.name}</strong>
+            <p className="mt-2 text-gray-600">{repository.description}</p>
+            <a
+                href={repository.html_url}
+                className="inline-block px-2 py-1 mt-2 font-bold text-gray-200 uppercase rounded-full bg-sky-500"
+            >
+                Ver
+            </a>
+        </li>
+    );
+}
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,22 +1,39 @@
+import { useEffect, useState } from 'react';
+import RepositoryItem from './RepositoryItem';
+
 export default function RepositoryList() {
+    const repository = {
+        name: 'React-Tailwind-Template',
+        description:
+            'Speed up your React.js and TailwindCSS project setup using this simple template',
+        link: 'https://github.com/devguilhermy/react-tailwind-template',
+    };
+
+    const [repoList, setRepoList] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/devguilhermy/repos')
+            .then((response) => response.json())
+            .then((data) => setRepoList(data));
+    }, []);
+
     return (
-        <div className="">
-            <h2 className="text-2xl text-blue-500 font-bold">
+        <section className="m-10">
+            <h2 className="text-2xl font-bold text-blue-500">
                 Lista de repositórios
             </h2>
 
-            <ul>
-                <li>
-                    <strong>NPM Expansions</strong>
-                    <p>Alternative text to Node Package Manager</p>
-                    <a href="/">Ver</a>
-                </li>
-                <li>
-                    <strong>NPM Expansions</strong>
-                    <p>Alternative text to Node Package Manager</p>
-                    <a href="/">Ver</a>
-                </li>
+            <ul className="grid gap-4 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                {repoList.map((repo) => {
+                    return (
+                        <RepositoryItem
+                            repository={repo}
+                            className=""
+                            key={repository.name}
+                        />
+                    );
+                })}
             </ul>
-        </div>
+        </section>
     );
 }
